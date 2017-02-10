9321cb2f4c21f9c38d23fe9132a47537de457d18
Josh Buckland <buck06191@gmail.com>
2017-02-10 13:31:42 +0000

    updated to include correct testing options;

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+import os
 from flask import Flask
-app = Flask(__name__)
 
+
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+print("Environment is {}".format(os.environ['APP_SETTINGS']))
 
 @app.route('/')
 def hello():
