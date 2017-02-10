685f6e63062fcac3b7cefec40aa661c1c6fcb88a
Josh Buckland <buck06191@gmail.com>
2017-02-10 14:46:39 +0000

    set mongodb uri to env var

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = os.environ['SECRET_KEY']
+    MONGOLAB_DB_URI = os.environ['MONGODB_URI']
 
 
 class ProductionConfig(Config):
