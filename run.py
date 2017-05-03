68314f4f23f341153061e37b2017a434dcc2bc21
Josh Buckland <buck06191@gmail.com>
2017-05-03 12:50:59 +0100

    ip problem fixed

--- run.py
+++ run.py
@@ -3,8 +3,6 @@ import pprint
 
 if __name__ == '__main__':
     try:
-        pprint.pprint(app.config)
         app.run(host=app.config['HOST'])
     except AttributeError:
-        print("CONFIG.HOST not found")
         app.run()
