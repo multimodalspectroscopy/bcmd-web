6dcd4e3e54f24145939df8a0e7d150582db32b12
Josh Buckland <buck06191@gmail.com>
2017-05-03 12:47:22 +0100

    debugging ip

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 from app import app
+import pprint
 
 if __name__ == '__main__':
     try:
-        print(app.config.HOST)
+        pprint.pprint(app.config)
         app.run(host=app.config.HOST)
     except AttributeError:
         app.run()
