55e5569d7c63210369446188ae9ee6e8306be8e1
Josh Buckland <buck06191@gmail.com>
2017-05-03 12:13:35 +0100

    find base dir set to verbose temproarily

--- app/bayescmd/util.py
+++ app/bayescmd/util.py
@@ -16,11 +16,12 @@ def findBaseDir(basename, max_depth=5, verbose=True):
     print("STARTING AT: %s\n Looking for: %s" % (BASEDIR, basename))
     for level in range(MAX_DEPTH):
         if verbose:
-            print('LEVEL %d: %s' % (level, BASEDIR))
+            print('LEVEL %d: %s\n Current basename: %s' %
+                  (level, BASEDIR, os.path.basename(BASEDIR)))
         if os.path.basename(BASEDIR) == basename:
             break
         else:
             BASEDIR = os.path.abspath(os.path.dirname(BASEDIR))
-        if level == MAX_DEPTH-1:
+        if level == MAX_DEPTH - 1:
             sys.exit('Could not find correct basedir\n Currently at %s' % BASEDIR)
     return os.path.relpath(BASEDIR)
